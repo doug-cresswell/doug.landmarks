08c4171fcc4eb4b8042dd8addb3291a4932c2d9c
Doug Cresswell <104023005+doug-cresswell@users.noreply.github.com>
2023-03-29 12:01:58 +0100

    Ad user profile and edit mode

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,37 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Doug C on 29/03/2023.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    @Environment(\.editMode) var editMode
+    @EnvironmentObject var modelData: ModelData
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            HStack {
+                Spacer()
+                EditButton()
+            }
+            
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                Text("Profile Editor")
+            }
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+            .environmentObject(ModelData())
+    }
+}
